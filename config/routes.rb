95b02b01fcfb1d1e095f987d31c5af7c72ba94b6
Dounx <imdounx@gmail.com>
2019-03-01 11:37:15 +0800

    add a backgroud job for feed's parsering

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :items
   resources :feeds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
 end
