c2a024ec060282a0f098088ccc4af9173630f6d1
Dounx <imdounx@gmail.com>
2019-03-13 11:35:09 +0800

    set sidekiq only admin can access

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,7 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   require 'sidekiq/cron/web'
 
-  mount Sidekiq::Web => '/sidekiq'
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
