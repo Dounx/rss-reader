3d005e90b9570aa34312c8d8daaabe2013f8d566
Dounx <imdounx@gmail.com>
2019-03-13 13:25:52 +0800

    limit index length

--- db/migrate/20190305072848_add_unique_index.rb
+++ db/migrate/20190305072848_add_unique_index.rb
@@ -2,7 +2,7 @@ class AddUniqueIndex < ActiveRecord::Migration[5.2]
   def change
     remove_index :items, :link
     remove_index :feeds, :link
-    add_index :items, :link, unique: true
-    add_index :feeds, :link, unique: true
+    add_index :items, :link, unique: true, :length => 100
+    add_index :feeds, :link, unique: true, :length => 100
   end
 end
