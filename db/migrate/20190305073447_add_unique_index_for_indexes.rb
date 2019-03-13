d367f7a7d15b2bde66912a2ea140fa9c65f3af15
Dounx <imdounx@gmail.com>
2019-03-13 13:28:36 +0800

    change doule index length limit

--- db/migrate/20190305073447_add_unique_index_for_indexes.rb
+++ db/migrate/20190305073447_add_unique_index_for_indexes.rb
@@ -1,6 +1,6 @@
 class AddUniqueIndexForIndexes < ActiveRecord::Migration[5.2]
   def change
-    add_index :subscriptions, [:user_id, :feed_id], unique: true, :length => 100
-    add_index :item_states, [:user_id, :item_id], unique: true, :length => 100
+    add_index :subscriptions, [:user_id, :feed_id], unique: true
+    add_index :item_states, [:user_id, :item_id], unique: true
   end
 end
