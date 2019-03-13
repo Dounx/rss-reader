e2735b6daee529418e65b6cbdf36753844dbc335
Dounx <imdounx@gmail.com>
2019-03-13 13:30:55 +0800

    change doule index length limit

--- db/migrate/20190311024347_change_title_uniqueness_in_feeds.rb
+++ db/migrate/20190311024347_change_title_uniqueness_in_feeds.rb
@@ -1,5 +1,5 @@
 class ChangeTitleUniquenessInFeeds < ActiveRecord::Migration[5.2]
   def change
-    add_index :feeds, :title, unique: true
+    add_index :feeds, :title, unique: true, :length => 100
   end
 end
