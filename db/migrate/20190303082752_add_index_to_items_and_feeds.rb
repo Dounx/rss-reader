a63ef51902fd64ab150fb83eca2a55a005b520f5
Dounx <imdounx@gmail.com>
2019-03-13 13:19:48 +0800

    index length linmit

--- db/migrate/20190303082752_add_index_to_items_and_feeds.rb
+++ db/migrate/20190303082752_add_index_to_items_and_feeds.rb
@@ -1,6 +1,6 @@
 class AddIndexToItemsAndFeeds < ActiveRecord::Migration[5.2]
   def change
-    add_index :feeds, :link
-    add_index :items, :link
+    add_index :feeds, :link, :length => 100
+    add_index :items, :link, :length => 100
   end
 end
